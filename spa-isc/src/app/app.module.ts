341e542be823a5e122f0f3d1a5f9a6181d418a29
Moaz Home <Moaz@homePc.com>
2020-02-13 05:24:04 -0800

    add all component main

--- spa-isc/src/app/app.module.ts
+++ spa-isc/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
+import { ServersComponent } from './component/servers/servers.component';
+import { AboutComponent } from './component/about/about.component';
+import { BlogComponent } from './component/blog/blog.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ServersComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
