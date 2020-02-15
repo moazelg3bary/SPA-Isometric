a4f458395863debd6cfb08d1b21ce410d9142485
moaz_home <moazHome@gmail.com>
2020-02-14 23:20:22 -0800

    add navbar fixed

--- spa-isc/src/app/app.component.ts
+++ spa-isc/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  scrollDown;
+
   constructor() {
+    window.onscroll = () => {
+      this.scrollDown = window.pageYOffset;
+      console.log(this.scrollDown)
+    }
   }
 }
