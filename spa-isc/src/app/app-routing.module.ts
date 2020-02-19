63d6dd197c30e346b4ff3c93df6c37938471e7dc
moaz_home <moazHome@gmail.com>
2020-02-18 23:54:05 -1200

    add to animations component

--- spa-isc/src/app/app-routing.module.ts
+++ spa-isc/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component'
 import { HomeComponent } from './component/home/home.component';
 import { ServersComponent } from './component/servers/servers.component';
 import { AboutComponent } from './component/about/about.component';
@@ -8,11 +9,11 @@ import { ContactComponent } from './component/contact/contact.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'serves', component: ServersComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'blog', component: BlogComponent},
-  { path: 'Page', component: ContactComponent}
+  {path: '', component: HomeComponent, data: {index: 0}},
+  {path: 'serves', component: ServersComponent, data: {index: 1}},
+  {path: 'about', component: AboutComponent, data: {index: 2}},
+  {path: 'blog', component: BlogComponent, data: {index: 3}},
+  { path: 'Page', component: ContactComponent, data: {index: 4}}
 ];
 
 @NgModule({
