eddbb4f02baa8b7b7c736c8ca8cb4f06f0c2c5d0
moaz_home <moazHome@gmail.com>
2020-02-22 04:21:35 -1200

    create prod-ar

--- spa-isc/src/app/component/home/home.component.ts
+++ spa-isc/src/app/component/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', '../../app.component.ar.css']
 })
 export class HomeComponent implements OnInit {
 
--- spa-isc/src/app/component/servers/servers.component.ts
+++ spa-isc/src/app/component/servers/servers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css', '../../app.component.ar.css']
 })
 export class ServersComponent implements OnInit {
 
